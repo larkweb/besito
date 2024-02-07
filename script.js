5c2aac6f52d6460ea314d48620236cc5114302d3
larkweb <148163767+larkweb@users.noreply.github.com>
2024-02-07 07:50:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "Te amo shey";
+  buttonsContainer.classList.add("hidden");
+  showLoveImage();
+  // Oculta la imagen principal
+  catImg.style.display = "none";
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "¿Está segura?",
+    "shey por favor",
+    "No me hagas esto :(",
+    "Estás rompiendo mi corazón",
+    "Te doy una oportunidad más",
+    "Voy a llorar...",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
+
+function showLoveImage() {
+  const loveImage = document.createElement("img");
+  loveImage.src = "img/love.gif";
+  loveImage.alt = "Love GIF";
+  loveImage.style.width = "200px"; // Ajusta el tamaño según sea necesario
+  loveImage.style.height = "200px"; // Ajusta el tamaño según sea necesario
+
+  // Inserta la imagen de amor después de la primera imagen
+  catImg.insertAdjacentElement("afterend", loveImage);
+
+  // Crea un párrafo para el mensaje "Yayyy!! :3"
+  const yayMessage = document.createElement("p");
+  yayMessage.textContent = "";
+
+  // Inserta el mensaje después de la segunda imagen
+  loveImage.insertAdjacentElement("afterend", yayMessage);
+}
